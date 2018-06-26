85b92f3a0dcb2e26f1f3e3764a2efc1ac29448f0
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 15:16:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,3 +86,19 @@ def all_holidays_with_bbq(holiday_hash)
   }
   holidays
 end
+
+holiday_hash = {
+   :winter => {
+     :christmas => ["Lights", "Wreath"],
+     :new_years => ["Party Hats"]
+   },
+   :summer => {
+     :fourth_of_july => ["Fireworks", "BBQ"]
+   },
+   :fall => {
+     :thanksgiving => ["Turkey"]
+   },
+   :spring => {
+     :memorial_day => ["BBQ"]
+   }
+ }
