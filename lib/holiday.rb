77ec5e6be332ae555093f3ce3a148bdc154dcf1a
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 15:16:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,8 +76,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holidays = []
   holiday_hash.each {|season, holiday|
-    holidays = []
     holiday.each {|holiday, supplies|
       if supplies.include?("BBQ")
         holidays.push(holiday)
