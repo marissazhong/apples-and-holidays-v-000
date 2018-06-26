9a955d99374919dd26e76ee819bf491694391e47
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 15:17:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,20 +87,20 @@ def all_holidays_with_bbq(holiday_hash)
   holidays
 end
 
-holiday_hash = {
-   :winter => {
-     :christmas => ["Lights", "Wreath"],
-     :new_years => ["Party Hats"]
-   },
-   :summer => {
-     :fourth_of_july => ["Fireworks", "BBQ"]
-   },
-   :fall => {
-     :thanksgiving => ["Turkey"]
-   },
-   :spring => {
-     :memorial_day => ["BBQ"]
-   }
- }
+#holiday_hash = {
+#   :winter => {
+#     :christmas => ["Lights", "Wreath"],
+#     :new_years => ["Party Hats"]
+#   },
+#   :summer => {
+#     :fourth_of_july => ["Fireworks", "BBQ"]
+#   },
+#   :fall => {
+#     :thanksgiving => ["Turkey"]
+#   },
+#   :spring => {
+#     :memorial_day => ["BBQ"]
+#   }
+#}
 
- puts(all_holidays_with_bbq(holiday_hash))
+# puts(all_holidays_with_bbq(holiday_hash))
