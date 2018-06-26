a5672d0baf505f43f5dbdb0ee182a7976179789d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 14:58:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each_with_index do |(holiday, supplies), index|
       string = "  #{holiday.capitalize}: "
       index != holiday.length-1 ? string << "#{supplies.capitalize}, " : string << "#{supplies.capitalize}"
+    end
+    holiday_hash
 end
 
 def all_holidays_with_bbq(holiday_hash)
